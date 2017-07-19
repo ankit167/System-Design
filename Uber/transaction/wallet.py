8cff483e6eae27b90a610a748be79f908f75f042
Ankit Agarwal <agrawal.coolankit@gmail.com>
2017-07-19 10:40:24 +0530

    Fixed spaces

--- Uber/transaction/wallet.py
+++ Uber/transaction/wallet.py
@@ -4,14 +4,14 @@ class Wallet:
     	self.__money = 0
     
     # Add money to wallet
-	def add_money(amount):
-		self.__money += amount
+    def add_money(amount):
+        self.__money += amount
     
     # Deduct money from wallet
-	def deduct_money(amount):
-		if self.__money >= amount:
-		    self.__money -= amount
+    def deduct_money(amount):
+        if self.__money >= amount:
+            self.__money -= amount
     
     # Return current money in wallet
-	def current_money(amount):
-		return self.__money
+    def current_money(amount):
+        return self.__money
